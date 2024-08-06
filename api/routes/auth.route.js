2b903ed1a1fa92ed3dcd4f8525ff33e87003a0d2
dinakaranthiru <dinakarant09@gmail.com>
2024-08-06 16:15:05 +0530

    create sign up api route

---
+++ api/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { signup } from '../controllers/auth.controller.js';
+
+
+
+const router = express();
+
+router.post("/signup", signup)
+
+export default router;
